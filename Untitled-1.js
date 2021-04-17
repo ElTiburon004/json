4fb64d48bde0825b9fab243c07c3fffabc9edc4d
JesusBejo <bejosantiago@hotmail.com>
2021-04-17 18:07:41 +0200

    primeiro

---
+++ Untitled-1.js
@@ -0,0 +1,17 @@
+/* for(let item of)
+  	}
+  }
+  }
+
+}
+
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myArr[0];
+  }
+};
+xmlhttp.open("GET", "json_demo_array.txt", true);
+xmlhttp.send(); */
